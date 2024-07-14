b5ab2d68607dde79b54b0f6e9e97eae9ce753b38
Ravi Shrestha <ravistha869@gmail.com>
2024-07-14 23:43:36 +0545

    ...

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,10 @@ import { ConfigModule } from "@nestjs/config"
 import { ArtistModule } from './artist/artist.module';
 import { RoleModule } from './role/role.module';
 import { MovieModule } from './movie/movie.module';
+import { GenreModule } from './genre/genre.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }),
-    AuthModule, PrismaModule, ArtistModule, RoleModule, MovieModule],
+    AuthModule, PrismaModule, ArtistModule, RoleModule, MovieModule, GenreModule],
 })
 export class AppModule { }
